f958af1346d299d92cbb768e330d244ef3bfda31
MarcArcherPingol <MarcArcherPingol@SMU-0044>
2025-04-08 18:22:39 +0800

    update

--- UserService/UserWriteService.cs
+++ UserService/UserWriteService.cs
@@ -37,7 +37,7 @@ namespace Exam.UserManager.Service
 
                 //***
                 //TODO Item 2: Implement the logic to update user
-                bool result = false; //result of update from user repository
+                bool result = _userRepository.Update(user); //result of update from user repository
                 //***
 
                 return result;
